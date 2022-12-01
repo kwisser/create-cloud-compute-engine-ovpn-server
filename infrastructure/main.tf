5ce50962fef9a18dbbabcbb4cf14406a7adf01c9
Klemens <wisserklemens@gmail.com>
2022-12-01 23:21:02 +0100

    removed white spaces

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,7 +15,6 @@ resource "google_compute_instance" "default" {
     }
   }
 
-
   network_interface {
     network = "default"
   
@@ -23,19 +22,17 @@ resource "google_compute_instance" "default" {
       // Ephemeral public IP
     }
   }
-
   metadata_startup_script = "sudo apt-get update && sudo apt-get upgrade -y && curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh chmod +x openvpn-install.sh && sudo AUTO_INSTALL=y ./openvpn-install.sh"
 }
 
 resource "google_compute_firewall" "default" {
   project = var.project_id
-  name    = "ovpn-firewall"
+  name    = "openvpn"
   network = "default"
 
   allow {
     protocol = "udp"
     ports    = ["1194"]
   }
-  
   source_ranges = ["0.0.0.0/0"]
 }
