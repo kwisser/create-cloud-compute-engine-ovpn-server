2374f66fe3412381f67df76f0a66765559bc7f1c
Klemens <wisserklemens@gmail.com>
2022-12-01 21:37:23 +0100

    Added Compute Engine Ressource

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "google_compute_instance" "default" {
+  project = "host-openvpn-server"
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+}
---
+++ provider.tf
@@ -0,0 +1,11 @@
+terraform {
+    required_providers {
+    google = {
+    source  = "hashicorp/google"
+    version = "4.44.1"
+    }
+}
+}
+
+provider "google" {
+}
