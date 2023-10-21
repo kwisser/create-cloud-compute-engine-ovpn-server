2adaceb9260791b02f4faee315d9405a5a5279ca
Klemens <wisserklemens@gmail.com>
2023-10-21 08:57:16 +0200

    removed default project

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,6 @@
 variable "project_id" {
     description = "Project ID of the Project where the VM should be deployed"
     type = string
-    default = "host-openvpn-server"
 }
 
 variable "name" {
