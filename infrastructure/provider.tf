2062b6418f3c5c94d3281ec33ff65d87ac856989
Klemens <wisserklemens@gmail.com>
2023-10-21 22:06:00 +0200

    updated provider and set project_id in google provider

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,10 +2,11 @@ terraform {
     required_providers {
     google = {
     source  = "hashicorp/google"
-    version = "4.44.1"
+    version = "5.2.0"
     }
 }
 }
 
 provider "google" {
+    project = var.project_id
 }
