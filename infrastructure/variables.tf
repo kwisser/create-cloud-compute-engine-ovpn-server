5dfc6e7bc6694770635fa4131bf767e43864c541
Klemens <54818308+kwisser@users.noreply.github.com>
2023-10-29 14:44:09 +0100

    Update variables.tf - add variable vm_username

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,6 @@
 variable "project_id" {
     description = "Project ID of the Project where the VM should be deployed"
     type = string
-    default = "host-openvpn-server"
 }
 
 variable "name" {
@@ -18,5 +17,9 @@ variable "machine_type" {
 variable "zone" {
     description = "Zone of the VM Instance"
     type        = string
-    default     = "europe-west1-b"
+}
+
+variable "vm_username" {
+    description = "Username of the user that will be created on the VM"
+    type        = string
 }
